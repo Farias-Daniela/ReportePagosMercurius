ec845d413fd313494c5ddb5c1ccf3f4dbd5a00c7
Daniela Farias <perdomoleidy1204@gmail.com>
2025-08-22 12:48:05 -0300

    progreso

---
+++ upload/views.py
@@ -0,0 +1,26 @@
+# uploads/views.py
+from django.shortcuts import render
+from .forms import UploadForm
+from .models import Upload
+
+def upload_view(request):
+    uploaded = None
+
+    if request.method == 'POST':
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            uploaded = form.save()
+            # Resetea el form después de guardar
+            form = UploadForm()
+    else:
+        form = UploadForm()
+
+    # muestra últimos 5 archivos
+    last_uploads = Upload.objects.order_by('-uploaded_at')[:5]
+
+    return render(request, 'upload.html', {
+        'form': form,
+        'uploaded': uploaded,
+        'uploads': last_uploads
+    })
+
